d185246e520bc5c58c8c9f4529ad3d1e19be1574
Julian Gaitan <juliangaitan_h@hotmail.com>
2023-07-21 23:36:39 -0500

    implement top down movement

--- Dimensional Dream/Assets/Main Character/Scripts/TopDownViewCharacterController.cs
+++ Dimensional Dream/Assets/Main Character/Scripts/TopDownViewCharacterController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,8 @@ public class TopDownViewCharacterController : MonoBehaviour
 
     private Vector2 _playerMovementInput;
 
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _rotateSpeed;
     private void Awake()
     {
         _playerControls = new PlayerInputActions();
@@ -34,5 +37,31 @@ public class TopDownViewCharacterController : MonoBehaviour
         _playerMovementInput = _move.ReadValue<Vector2>();
         
         var _movementTarget = new Vector3 (_playerMovementInput.x, 0 , _playerMovementInput.y);
+
+        var _movementVector = MoveTowardsTarget(_movementTarget);
+
+        RotateTowardsMovementVector(_movementVector);
     }
+
+    private void RotateTowardsMovementVector(Vector3 _movementVector)
+    {
+        if (_movementVector.magnitude == 0) return;
+        var _rotation = Quaternion.LookRotation(_movementVector);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, _rotation, _rotateSpeed);
+    }
+
+    private Vector3 MoveTowardsTarget(Vector3 _target)
+    {
+        var _speed = _moveSpeed * Time.deltaTime;
+
+        _target = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * _target;
+
+        var _targetPoistion = transform.position + _target * _speed;
+        
+        transform.position = _targetPoistion;
+
+        return _target;
+    }
+
 }
